25b1635597aefd0dbf5b6a221fa92816e5f1f098
Michael <mdurfey@me.com>
2017-02-03 01:51:29 -0800

    changed index.html hello world

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
-console.log("Hello World");
-alert("hi");
+$.get("https://www.reddit.com/r/awww.json",(data)=>{
+
+
+	var redditPosts = data.data.children;
+
+	redditPosts.forEach((post, i)=> {
+		console.log(post);
+		if(i =! 0){
+			var imageUrl = post.data.preview.images[0].source.url
+			$('body').append(imgCreator(imageUrl));
+		}
+	})
+});
+
+let imgCreator = (url) => `<img src="${url}"></img>`;
