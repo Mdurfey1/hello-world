480fb9fa09974f437efea276cc04cb360ab7b1ae
Michael <mdurfey@me.com>
2017-02-04 13:09:24 -0800

    npm init

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-$.get("https://www.reddit.com/r/awww.json",(data)=>{
 
 
+$.get("https://www.reddit.com/r/earthporn/new.json",(data)=>{
+	
 	var redditPosts = data.data.children;
 
-	redditPosts.forEach((post, i)=> {
-		console.log(post);
+	redditPosts.forEach((post, i)=> {		
 		if(i =! 0){
 			var imageUrl = post.data.preview.images[0].source.url
 			$('body').append(imgCreator(imageUrl));
